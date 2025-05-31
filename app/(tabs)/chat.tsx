c04cca8408a4800d0d5b058069f0d9670c8663d2
Puggy Potato <qingyaurou@gmail.com>
2025-05-31 22:15:31 +0800

    Added Tabs to the screen, private message and group message

---
+++ app/(tabs)/chat.tsx
@@ -0,0 +1,28 @@
+import { Text, View , StyleSheet } from "react-native";
+import {Link} from "expo-router"
+
+export default function Index() {
+  return (
+    <View
+      style={styles.view}>
+      <Link href="/groupChat" style={styles.group}>Group Chat</Link>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  view:{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+  },
+  group:{
+        width: 100,
+        height:20,
+        backgroundColor:"coral", 
+        borderRadius :8,
+        textAlign:"center",
+        alignItems:"center",
+        display:"flex"}
+})
---
+++ app/(tabs)/groupChat.tsx
@@ -0,0 +1,9 @@
+import {View,Text} from "react-native"
+export default function LoginScreen(){
+    return(
+        <View>
+            {""}
+            <Text>Hello This Is The Group Chat Page</Text>
+        </View>
+    )
+}
